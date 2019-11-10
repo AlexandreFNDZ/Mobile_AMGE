5d5095e185c833cbeb0b33b1c8c6f2faad1b8fbb
Alexandre Fernandes <alexandre_201@hotmail.com>
2019-11-10 16:18:20 -0300

    Adicionado layout Menu

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class Menu extends StatefulWidget {
@@ -8,16 +10,113 @@ class Menu extends StatefulWidget {
 class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.white,
-      child: Center(
-        child: Text(
-          "MENU",
-          style: TextStyle(color: Colors.blue, fontSize: 50),
+    return new Scaffold(
+      backgroundColor: Colors.white.withOpacity(0.8),
+      body: new Stack(
+      children: <Widget>[
+        ClipPath(
+          child: Container(color: Colors.blueAccent),
+          clipper: GetClipper(),
         ),
-      ),
-    );
+        Positioned(
+          width: MediaQuery.of(context).size.width,
+          top: MediaQuery.of(context).size.height / 5,
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 150.0,
+                height: 150.0,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/logo.png"),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                  boxShadow: [BoxShadow(blurRadius: 7.0, color: Colors.black)],
+                ),
+              ),
+              SizedBox(height: 90.0),
+              Text(
+                'Tom Cruise',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto'),
+              ),
+              SizedBox(height: 15.0),
+              Text(
+                'Subscribe guys',
+                style: TextStyle(
+                    fontSize: 17.0,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: 'Roboto'),
+              ),
+              SizedBox(height: 25.0),
+              Container(
+                height: 40.0,
+                width: 95.0,
+                child: Material(
+                  borderRadius: BorderRadius.circular(20.0),
+                  shadowColor: Colors.black,
+                  color: Colors.blue,
+                  elevation: 7.0,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Center(
+                      child: Text(
+                        'Editar Foto',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'Roboto'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.0),
+              Container(
+                height: 40.0,
+                width: 95.0,
+                child: Material(
+                  borderRadius: BorderRadius.circular(20.0),
+                  shadowColor: Colors.black,
+                  color: Colors.blue,
+                  elevation: 7.0,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.maybePop(context);
+                    },
+                    child: Center(
+                      child: Text(
+                        'Sair',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'Roboto'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
+
+class GetClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height / 1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
   }
 }
